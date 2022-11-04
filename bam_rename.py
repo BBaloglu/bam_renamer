9031e71efbaea9203d370352c1d88d64e37a7e0a
Bilge <bilge@bilge>
2022-11-03 17:00:49 -0700

    test

--- bam_rename.py
+++ bam_rename.py
@@ -21,30 +21,38 @@ def get_sample_name(fname):
             out.append([i, j]) 
     return out
 
-def rename_file(infile, outfolder):
-    infile = get_sample_name(infile)
-
+def rename_file(infolder, outfolder, sample_list):
+    # make output directory if it does not exist
     if os.path.exists(outfolder)==False:
         cmd = 'mkdir '+outfolder
         print(cmd)
         subprocess.call(cmd, shell=True)
 
-    for f1, f2 in infile:
+    # iterate thru file list
+    for f1, f2 in sample_list:
         print('working on', f1, f2)
-        outname = outfolder+'/'+f1+'_'+f2
-        outname = outname.replace(' ','_')
-        cmd = 'touch '+outname
-        print(cmd)
-        subprocess.call(cmd, shell=True)
+        # get list of bam files
+        blist = glob.glob(infolder+'/'+f2+'*.bam')
+        print(blist)
+        # rename bam file and copy over
+        for bfile in blist:
+            outname = outfolder+'/'+f1+'_'+bfile
+            outname = outname.replace(' ','_')
+            cmd = 'touch '+outname
+            print(cmd)
+            subprocess.call(cmd, shell=True)
+            cmd = 'cp '+bfile+' '+outname
 
 def main():
     parser = argparse.ArgumentParser(description='Script to rename bam files based on sample name', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-c', dest='run_params', type=str, help='file with run parameters')
     parser.add_argument('-i', dest='infile', type=str, help='input folder')
     parser.add_argument('-o', dest='outfile', type=str, help='destination folder')
 
     args = parser.parse_args()
     print(args)
-    rename_file(args.infile, args.outfile)
+    flist = get_sample_name(args.run_params)
+    rename_file(args.infile, args.outfile, flist)
 
 if __name__ == "__main__":
     main()
